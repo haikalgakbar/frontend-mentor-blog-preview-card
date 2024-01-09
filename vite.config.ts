0cb33946a767d2f683fe47a7fab70a19c5924914
gibran <haikalgakbar@gmail.com>
2024-01-09 18:22:41 +0700

    change vite config base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/<REPO>/',
   plugins: [react()],
+  base: '/frontend-mentor-blog-preview-card/',
 })
